5c4106a483dd9b3be8edf2d8a4cb02e2cd02f6ee
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-30 16:23:58 +0500

    error resolve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./components/blog/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/header/navbar";
+import Navbar from "./components/header/Navbar";
 import Footer from "./components/footer/Footer";
 import Signup from "./components/user/Signup";
 import Signin from "./components/user/Signin";
