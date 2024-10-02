f3b9b66af8237ff14b87a59a74478ef9724fa5df
Abdurazaq333 <irazaqr986@gmail.com>
2024-10-02 16:36:32 +0500

    user registered successfully

--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -1,18 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSignUpMutation } from "../../redux/api/userSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [image, setImage] = useState(null);
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const fileHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const [signUp, { isError, isLoading, error }] = useSignUpMutation();
+  const submitUserData = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("name", userData.name);
+    formData.append("email", userData.email);
+    formData.append("password", userData.password);
+    if (image) {
+      formData.append("image", image);
+    }
+
+    try {
+      const res = await signUp(formData).unwrap();
+      navigate("/");
+      toast.success(res?.message, {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="bg-gray-300 rounded-md  w-full h-full  md:w-[30%] mx-auto p-7 text-sm mt-32 mb-12 space-y-5">
       <h1 className="text-center font-bold text-xl">SignUp</h1>
-      <form onSubmit={""} className="flex flex-col space-y-4 rounded-md ">
-        <input type="file" />
-        <input className="p-1 rounded-md" type="text" placeholder="name" />
-        <input className="p-1 rounded-md" type="text" placeholder="email" />
-        <input className="p-1 rounded-md" type="text" placeholder="password" />
+      <form
+        onSubmit={submitUserData}
+        className="flex flex-col space-y-4 rounded-md "
+      >
+        <input type="file" name="file" onChange={fileHandler} />
+        <input
+          className="p-1 rounded-md"
+          type="text"
+          name="name"
+          placeholder="name"
+          value={userData.name || ""}
+          onChange={changeHandler}
+        />
+        <input
+          className="p-1 rounded-md"
+          type="text"
+          name="email"
+          placeholder="email"
+          value={userData.email || ""}
+          onChange={changeHandler}
+        />
+        <input
+          className="p-1 rounded-md"
+          type="text"
+          name="password"
+          placeholder="password"
+          value={userData.password || ""}
+          onChange={changeHandler}
+        />
 
-        <button className="bg-white rounded-md p-1 hover:font-semibold">
-          SignUp
+        <button
+          type="submit"
+          className="bg-white rounded-md p-1 hover:font-semibold"
+          disabled={isLoading}
+        >
+          {isLoading ? "SignUp..." : "SignUp"}
         </button>
         <p className="text-center">
           if u have already an account{" "}
--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -1,9 +1,9 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const query = fetchBaseQuery({ baseUrl: "http://localhost:5173/" });
+const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8005/api" });
 
 export const apiSlice = createApi({
-  query,
+  baseQuery,
   tagTypes: ["User", "Blog"],
   endpoints: () => ({}),
 });
